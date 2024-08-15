42feb467d5f69a73153a73804817ab84ffb6a59b
Anonymouspersonx <w.xia@tudelft.nl>
2024-08-15 14:03:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='gmm_trans',  # Replace with your package name
     version='0.1.0',  # Initial version
-    author='Weijie Xia',
-    author_email='xiaweijie1996@gmail.com',
+    author='Anonymouspersonx',
+    author_email='Anonymouspersonx Gmail',
     description='An Efficient and Explainable Transformer-Based Few-Shot Learning for Modeling Electricity Consumption Profiles Across Thousands of Domains',
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',  # Use 'text/markdown' if README.md is in Markdown
-    url='https://github.com/xiaweijie1996/TransformerEM-GMM.git',  # URL of your project's repository
+    long_description_content_type='text/markdown',  
+    url='https://github.com/Anonymouspersonx/TransformerEM-GMM.git',  
     packages=find_packages(),  # Automatically find packages in the project
     install_requires=[
         'torch==2.1.2',
